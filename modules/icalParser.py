1d580db777e4027c04b2a6683713af9fe7a7cdc5
bnm <57193455+kyanbasu@users.noreply.github.com>
2026-02-23 14:33:11 +0100

    unknown info warning

--- modules/icalParser.py
+++ modules/icalParser.py
@@ -49,16 +49,29 @@ def parseIcal(name):
 
     lines = f.read().splitlines()
 
+    unknowns = {}
+
     for line in lines:
         #print(lineIndex, lines[lineIndex])
         if line.startswith("BEGIN:VEVENT"):
             currentEventIndex += 1
-            returnOBJ["events"].append({})
+            returnOBJ["events"].append({
+                "room": "?",
+                "building": "?",
+                "location": "?",
+            })
         elif line.startswith("END:VEVENT"):
             eventPostProcess(returnOBJ["events"][currentEventIndex], currentEventIndex)
+            unknown = []
+            for c in ["room", "building", "location"]:
+                if returnOBJ["events"][currentEventIndex][c] == "?": unknown.append(c)
+
+            if len(unknown) > 1 and not returnOBJ["events"][currentEventIndex]["summary"] in unknowns:
+                unknowns[returnOBJ["events"][currentEventIndex]["summary"]] = unknown
+
         elif line.startswith("SUMMARY"):
             returnOBJ["events"][currentEventIndex]["summary"] = "-".join(line.split("-")[1:])[1:]
-            returnOBJ["events"][currentEventIndex]["type"] = line.split(':')[1][0]
+            returnOBJ["events"][currentEventIndex]["type"] = line.split(':', 1)[1][0]
         elif line.startswith("DTSTART"):
             returnOBJ["events"][currentEventIndex]["dtstart"] = formatDate(line.split(":")[-1])
         elif line.startswith("DTEND"):
@@ -70,12 +83,12 @@ def parseIcal(name):
                 i += 1
             desc = ":".join(line.split(":")[1:])
             returnOBJ["events"][currentEventIndex]["description"] = desc.replace("\\n", "\n")
-            returnOBJ["events"][currentEventIndex]["room"] = desc.split("\\")[0].split(" ")[1]
-            returnOBJ["events"][currentEventIndex]["building"] = desc.split("[")[1].split("]")[0] if len(desc.split("[")) > 1 else None
+            if len(desc.split("\\", 1)[0].split(" ", 1)) > 1: returnOBJ["events"][currentEventIndex]["room"] = desc.split("\\", 1)[0].split(" ", 1)[1]
+            if len(desc.split("[", 1)) > 1: returnOBJ["events"][currentEventIndex]["building"] = desc.split("[", 1)[1].split("]", 1)[0]
         elif line.startswith("STATUS"):
             returnOBJ["events"][currentEventIndex]["status"] = line.split(":")[1]
         elif line.startswith("LOCATION"):
-            returnOBJ["events"][currentEventIndex]["location"] = "".join(line.split(":")[1].split("\\"))
+            if len(line.split(":", 1)) > 1: returnOBJ["events"][currentEventIndex]["location"] = "".join(line.split(":", 1)[1].split("\\"))
         elif line.startswith("DTSTAMP") and returnOBJ["dtstamp"] is None:
             returnOBJ["dtstamp"] = formatDate(line.split(":")[-1])
 
@@ -83,6 +96,11 @@ def parseIcal(name):
             returnOBJ["timezone"] = line.split(":")[1]
 
         lineIndex += 1
+
+    if len(unknowns) > 0:
+        print("WARNING> Couldn't get information about this events")
+        print(unknowns)
+        input("Continue?")
 
     eventindexes_to_remove.sort(reverse=True)
 
